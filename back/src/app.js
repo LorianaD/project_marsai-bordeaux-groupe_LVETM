dd30287fa34869efaeffbe6878f5ba849cd8f22f
dencorps-codeur <mickael.roze@laplateforme.io>
2026-01-29 14:49:42 +0100

    event model + controller + routes

--- back/src/app.js
+++ back/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // route test
-app.get("/", (res) => {
+app.get("/", (req,res) => {
   res.json({
     message: "the site is running"
   });
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { testController } from "../controllers/test.controller.js";
+import eventsRouter from "./eventsAdmin.js";
+
 
 
 const router = Router();
 
 router.get("/test", testController);
+router.use("/events", eventsRouter);
 
 export default router;
