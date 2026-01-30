cad37eb31b6d56ff710332e8c31f32c8f7d3e153
dencorps-codeur <mickael.roze@laplateforme.io>
2026-01-30 16:35:03 +0100

    front page agenda + icone + routes app.jsx model event index.html

--- back/src/models/event.js
+++ back/src/models/event.js
@@ -2,14 +2,14 @@ import { pool } from "../db/index.js";
 
 // Récupérer tous les events (pour liste / admin)
 export const findAllEvents = async () => {
-  const [rows] = await pool.query(
-    "SELECT id, title, date, illustration, location FROM events ORDER BY date ASC"
+  const [rows] = await pool.execute(
+    "SELECT id, title, description, date, length, stock, illustration, location FROM events ORDER BY date ASC"
   );
   return rows;
 };
 
 // Récupérer un event par son id (pour page détail / admin)
 export const findEventById = async (id) => {
-  const [rows] = await pool.query("SELECT * FROM events WHERE id = ?", [id]);
+  const [rows] = await pool.execute("SELECT * FROM events WHERE id = ?", [id]);
   return rows[0] || null;
 };
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,19 +1,20 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import MainLayout from './components/Layout/MainLayout.jsx'
-import Home from './pages/Home.jsx'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./components/Layout/MainLayout.jsx";
+import Home from "./pages/Home.jsx";
+import Events from "./pages/Events.jsx";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route element={<MainLayout/>}>
-          < Route path='/' element={<Home/>}/>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
