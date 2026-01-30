053b8fbec6dc7ddd338546f233004231a88f9037
LorianaD <loriana.diano@gmail.com>
2026-01-30 16:06:42 +0100

    finish Home/SectionLocalisation

---
+++ front/src/components/Maps/Localisation.jsx
@@ -0,0 +1,15 @@
+function Localisation() {
+    return(
+        <div className="">
+            <iframe
+                title="Google Map"
+                className="flex w-[974px] h-[547px] p-[10px] items-start gap-[10px] aspect-[276/155] rounded-[40px]"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8044.241161952438!2d5.362378323117001!3d43.314391970182314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c13ddc0211b9%3A0xd1642ae4b32c4bc4!2s%C3%89cole%20La%20Plateforme_%20Marseille%20-%20Entr%C3%A9e%20Sud!5e1!3m2!1sfr!2sfr!4v1769784840463!5m2!1sfr!2sfr"
+            ></iframe>
+        </div>
+    )
+}
+
+export default Localisation
