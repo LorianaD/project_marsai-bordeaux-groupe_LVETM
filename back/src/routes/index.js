cfb1689427629a548704435d525845d13d8bcb43
LorianaD <loriana.diano@gmail.com>
2026-02-03 16:32:22 +0100

    add in the back the partner/addPartnerController

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 // import { testController } from "../controllers/test.controller.js";
 import eventsRouter from "./eventsAdmin.js";
 import videosRouter from "./videos.js";
+import partner from "./partner.js";
 
 const router = Router();
 
 // router.get("/test", testController);
 router.use("/videos", videosRouter);
 router.use("/events", eventsRouter);
+router.use("/partner", partner)
 
 export default router;
