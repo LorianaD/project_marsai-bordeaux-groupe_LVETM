532d354fa0945c5fe7a4f05922de6626d0a8a80c
LorianaD <loriana.diano@gmail.com>
2026-02-04 14:52:42 +0100

    Finish GetAllPartner back and front

--- front/src/components/Home/SectionPartner.jsx
+++ front/src/components/Home/SectionPartner.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import GetPartnerApi from "../../services/Partner/GetPartnetApi";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function SectionPartner() {
 
     const { t } = useTranslation("home");
+
+    const [partners, setPartners] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [Message, setMessage] = useState("");
+
+    async function GetAllPartner() {
+        console.log("Fonction GetAllPartner OK");
+        
+        try {
+            console.log("try in the function GetAllPartner OK");
+            
+            setLoading(true);
+            setMessage("");
+
+            const res = await GetPartnerApi();
+            console.log("API response: ", res);
+            
+            setPartners(Array.isArray(res.data) ? res.data : []);
+
+        } catch (error) {
+            console.error(error);
+            setMessage("Erreur serveur");
+        }
+
+    }
+
+    useEffect(()=> {
+        GetAllPartner();
+    }, []);
 
     return(
         <section className="flex flex-col items-start gap-[48px] p-[20px] md:gap-[80px] md:px-[75px] md:py-[100px] self-stretch">
@@ -22,11 +54,13 @@ function SectionPartner() {
                 </h2>
             </div>
             <div className="grid md:h-[146px] grid-cols-2 md:grid-cols-4 gap-[16px] self-stretch">
-                <div className="flex items-center justify-center self-stretch justify-self-stretch row-span-1 col-span-1 rounded-[24px] border border-[rgba(128,128,128,0.05)] bg-[rgba(128,128,128,0.05)] dark:border-[rgba(255,255,255,0.05)] dark:bg-[rgba(255,255,255,0.05)]">
-                    <div className="h-[145px] w-[175px] md:w-[154px] md:h-[108px] bg-[url('src/assets/imgs/partnersLogo/psl.png')] bg-contain bg-no-repeat bg-center">
-                        {/* <img src="src/assets/imgs/partnersLogo/psl.png" alt="psl" /> */}
+                {partners.length > 0 && partners.map((p) => (
+                    <div key={ p.id ?? p.name } className="flex items-center justify-center self-stretch justify-self-stretch row-span-1 col-span-1 rounded-[24px] border border-[rgba(128,128,128,0.05)] bg-[rgba(128,128,128,0.05)] dark:border-[rgba(255,255,255,0.05)] dark:bg-[rgba(255,255,255,0.05)]">
+                        <div className="w-[175px] md:w-[154px] md:h-[108px] flex items-center justify-center">
+                            <img src={`${API_URL}${p.img}`} alt={p.name} />
+                        </div>
                     </div>
-                </div>        
+                ))}
             </div>
         </section>
     )
