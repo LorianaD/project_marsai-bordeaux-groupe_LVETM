26805eeb77f4772645e0baf3758b54b84c4470d2
LorianaD <loriana.diano@gmail.com>
2026-02-04 10:47:11 +0100

    Add Partner in the back

---
+++ back/src/models/partner/insert.model.js
@@ -0,0 +1,28 @@
+import { pool } from "../../db/index.js";
+
+async function insertPartner({ name, img, url }) {
+    console.log("model inserPartner OK");
+    
+    const query = `
+        INSERT INTO partner (name, img, url)
+        VALUES (?, ?, ?)
+    `;
+    console.log(query);
+
+    const values = [name, img, url];
+    console.log(values);
+    
+    
+    const [result] = await pool.execute(query, values);
+    console.log(result);
+    
+    return {
+        id: result.insertId,
+        name,
+        img,
+        url,
+    };
+
+}
+
+export default insertPartner
