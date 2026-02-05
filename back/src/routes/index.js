84941c08ed6ef75ae1b4411bcb4fe4c226d31db4
VanessDev <vanessa.biamonti@gmail.com>
2026-02-05 16:00:51 +0100

    NewsletterDone-MoreComments

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -3,18 +3,21 @@ import { Router } from "express";
 import eventsRouter from "./eventsAdmin.js";
 import videosRouter from "./videos.js";
 
-import contactRouter from "./contact.js"; 
+import contactRouter from "./contact.js";
 
 import partner from "./partner.js";
 
+import newsletterRoutes from "./newsletter.routes.js";
 
 const router = Router();
 
 // router.get("/test", testController);
 router.use("/videos", videosRouter);
 router.use("/events", eventsRouter);
-router.use("/partner", partner)
+router.use("/partner", partner);
 
 router.use("/", contactRouter);
+
+router.use("/", newsletterRoutes);
 
 export default router;
