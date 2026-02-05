faca828d14562a1701ba8bfaec45b031290c164d
LorianaD <loriana.diano@gmail.com>
2026-02-05 13:12:29 +0100

    Fix routes

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,14 +1,6 @@
 import { Router } from "express";
-import { testController } from "../controllers/test.controller.js";
-
-import usersRouter from "./users.js";
-
-const router = Router();
-
-router.get("/test", testController);
-router.use("/users", usersRouter);
-
 // import { testController } from "../controllers/test.controller.js";
+import usersRouter from "./users.js";
 import eventsRouter from "./eventsAdmin.js";
 import videosRouter from "./videos.js";
 import contactRouter from "./contact.js"; 
@@ -17,6 +9,8 @@ import partnerRouter from "./partner.js";
 const router = Router();
 
 // router.get("/test", testController);
+router.get("/test", testController);
+router.use("/users", usersRouter);
 router.use("/videos", videosRouter);
 router.use("/events", eventsRouter);
 router.use("/partner", partnerRouter)
