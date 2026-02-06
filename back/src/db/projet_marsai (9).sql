dbeb614a86a24806e87e3c0dc7e6312bca3eaeb4
AnthonyPailloux <anthony.pailloux@laplateforme.io>
2026-02-06 09:44:02 +0100

    update_file_bdd

--- back/src/db/projet_marsai (9).sql
+++ back/src/db/projet_marsai (9).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 04 fév. 2026 à 14:47
+-- Généré le : ven. 06 fév. 2026 à 08:38
 -- Version du serveur : 8.4.3
 -- Version de PHP : 8.3.16
 
@@ -216,13 +216,22 @@ CREATE TABLE `memo_selector` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `newsletter_subscriptions`
+-- Structure de la table `newsletter_subscribers`
 --
 
-CREATE TABLE `newsletter_subscriptions` (
+CREATE TABLE `newsletter_subscribers` (
   `id` int NOT NULL,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `unsubscribed_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `newsletter_subscribers`
+--
+
+INSERT INTO `newsletter_subscribers` (`id`, `email`, `created_at`, `unsubscribed_at`) VALUES
+(1, 'test123@gmail.com', '2026-02-05 14:33:17', NULL);
 
 -- --------------------------------------------------------
 
@@ -293,7 +302,10 @@ INSERT INTO `still` (`id`, `file_name`, `video_id`) VALUES
 (4, '1770037126201-841805-Sans_titre__Pr__sentation___5_.jpg', 6),
 (5, '1770037126205-322060-Capture_d___cran_2026-02-02_094320.png', 6),
 (6, '1770037126205-760925-MarsAI_Film_Festival-2026_ABOUT__1_.jpg', 6),
-(7, '1770207153926-434493-korog.art_An_hells_gate_--v_7_f639f163-0927-4d0d-8dbe-ac00144c3afb.png', 7);
+(7, '1770207153926-434493-korog.art_An_hells_gate_--v_7_f639f163-0927-4d0d-8dbe-ac00144c3afb.png', 7),
+(8, '1770288198692-531971-u7389435774_1977_London_punk_era_hyper-realistic_cinematic_arch_01ef2320-0074-44b4-8999-897af9a08efe.png', 8),
+(9, '1770290692244-408831-MarsAI_Film_Festival-2026_ABOUT__Pr__sentation_.jpg', 9),
+(10, '1770290989726-20633-ethopstudio_black_and_white_blurred_photograph_of_people_dancin_2df345e4-8c33-4c1d-8515-972758658476.png', 10);
 
 -- --------------------------------------------------------
 
@@ -319,6 +331,14 @@ CREATE TABLE `tags` (
   `name` varchar(80) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`, `created_at`) VALUES
+(1, 'science-fiction', '2026-02-05 10:43:20'),
+(2, 'bio', '2026-02-05 11:24:53');
 
 -- --------------------------------------------------------
 
@@ -374,18 +394,22 @@ CREATE TABLE `videos` (
   `ownership_certified` tinyint(1) NOT NULL DEFAULT '0',
   `ownership_certified_at` datetime DEFAULT NULL,
   `promo_consent` tinyint(1) NOT NULL DEFAULT '0',
-  `promo_consent_at` datetime DEFAULT NULL
+  `promo_consent_at` datetime DEFAULT NULL,
+  `youtube_upload_error` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `videos`
 --
 
-INSERT INTO `videos` (`id`, `youtube_video_id`, `video_file_name`, `title`, `title_en`, `synopsis`, `synopsis_en`, `cover`, `language`, `country`, `duration`, `tech_resume`, `ai_tech`, `creative_resume`, `email`, `director_name`, `director_lastname`, `director_gender`, `birthday`, `mobile_number`, `home_number`, `address`, `director_country`, `discovery_source`, `upload_status`, `created_at`, `featured`, `ownership_certified`, `ownership_certified_at`, `promo_consent`, `promo_consent_at`) VALUES
-(4, NULL, '1770022546953-878781-MarseillefutureGoodone3.mp4', 'Vue sur Marseille', 'View on Marseille', 'Magnifique vue sur Marseille', 'Beautiful view on Marseille', '1770022546993-180497-Sans_titre__Pr__sentation___2_.jpg', 'fr', 'FR', 120, 'sdkcjdkfjzekflnezfklnef', 'Midjourney', 'scklskclmqssqmds', 'vanessa.biamonti@laplate.com', 'Vanessa', 'Callea', 'Mrs', '1995-12-11', '+33650925487', '0556789876', '13 rue Exemple, Paris', 'FR', 'Facebook', 'Published', '2026-02-03 10:09:27', 0, 0, NULL, 0, NULL),
-(5, NULL, '1770025470430-682094-MarseillefutureGoodone2.mp4', 'Les beaux jours', 'Beautiful days', 'sdfjkzejfsdkslfsjdjfklsdfkl', 'qdflvdf;v,dslv,df;v', '1770025470480-428870-Sans_titre__Pr__sentation___3_.jpg', 'fr', 'France', 120, 'ksdcsdlcksdfl', 'Midjourney', 'wx wx;,', 'vanessa.biamonti@gmail.com', 'Julie', 'Callea', 'Mrs', '1977-07-21', NULL, NULL, '15 rue Exemple, Paris', 'France', 'Facebook', 'Published', '2026-02-03 10:09:27', 0, 0, NULL, 0, NULL),
-(6, NULL, '1770037126155-987006-MarseillefutureGoodone3.mp4', 'Video bebe', 'Video Baby', 'sd;v;dsvsd;:v', 'qslcqslmclmqsclmqs', '1770037126194-723445-Sans_titre__Pr__sentation___7_.jpg', 'fr', 'France', 60, 'djfsdjkfjsdfjkd', 'Midjourney', 'sd,c,sdqcf,sd,;sd,;', 'test2@mail.com', 'Biamonti', 'Callea', 'Mr', '2008-06-14', '0650925488', NULL, '16 rue Exemple, Paris', 'Australia', 'Facebook', 'Published', '2026-02-03 10:09:27', 0, 0, NULL, 0, NULL),
-(7, NULL, '1770207153587-937416-vanessdev_Marseille_in_a_desirable_future_black_and_white_cin_fc2947f3-1740-42ee-bfc4-aedeef052256_2.mp4', 'dsdsvds', 'ddcdssdvsd', 'qssqqs', 'sqqsqs', '1770207153812-118295-sankaku_john_baldessari_art_--v_7_7d750db0-c595-4d24-8364-1efbdeeeced4.png', 'nl', 'Anguilla', 60, 'xdsdsqsqqs', 'Midjourney', 'sddssd', 'vanessa.biamonti@laplate.com', 'Juliette', 'Ravioli', 'Mr', '1986-11-20', '0650925487', NULL, '14 rue Exemple, Paris', 'Antigua and Barbuda', 'Facebook', 'Pending', '2026-02-04 13:12:33', 0, 1, '2026-02-04 13:12:34', 1, '2026-02-04 13:12:34');
+INSERT INTO `videos` (`id`, `youtube_video_id`, `video_file_name`, `title`, `title_en`, `synopsis`, `synopsis_en`, `cover`, `language`, `country`, `duration`, `tech_resume`, `ai_tech`, `creative_resume`, `email`, `director_name`, `director_lastname`, `director_gender`, `birthday`, `mobile_number`, `home_number`, `address`, `director_country`, `discovery_source`, `upload_status`, `created_at`, `featured`, `ownership_certified`, `ownership_certified_at`, `promo_consent`, `promo_consent_at`, `youtube_upload_error`) VALUES
+(4, NULL, '1770022546953-878781-MarseillefutureGoodone3.mp4', 'Vue sur Marseille', 'View on Marseille', 'Magnifique vue sur Marseille', 'Beautiful view on Marseille', '1770022546993-180497-Sans_titre__Pr__sentation___2_.jpg', 'fr', 'FR', 120, 'sdkcjdkfjzekflnezfklnef', 'Midjourney', 'scklskclmqssqmds', 'vanessa.biamonti@laplate.com', 'Vanessa', 'Callea', 'Mrs', '1995-12-11', '+33650925487', '0556789876', '13 rue Exemple, Paris', 'FR', 'Facebook', 'Published', '2026-02-03 10:09:27', 0, 0, NULL, 0, NULL, NULL),
+(5, NULL, '1770025470430-682094-MarseillefutureGoodone2.mp4', 'Les beaux jours', 'Beautiful days', 'sdfjkzejfsdkslfsjdjfklsdfkl', 'qdflvdf;v,dslv,df;v', '1770025470480-428870-Sans_titre__Pr__sentation___3_.jpg', 'fr', 'France', 120, 'ksdcsdlcksdfl', 'Midjourney', 'wx wx;,', 'vanessa.biamonti@gmail.com', 'Julie', 'Callea', 'Mrs', '1977-07-21', NULL, NULL, '15 rue Exemple, Paris', 'France', 'Facebook', 'Published', '2026-02-03 10:09:27', 0, 0, NULL, 0, NULL, NULL),
+(6, NULL, '1770037126155-987006-MarseillefutureGoodone3.mp4', 'Video bebe', 'Video Baby', 'sd;v;dsvsd;:v', 'qslcqslmclmqsclmqs', '1770037126194-723445-Sans_titre__Pr__sentation___7_.jpg', 'fr', 'France', 60, 'djfsdjkfjsdfjkd', 'Midjourney', 'sd,c,sdqcf,sd,;sd,;', 'test2@mail.com', 'Biamonti', 'Callea', 'Mr', '2008-06-14', '0650925488', NULL, '16 rue Exemple, Paris', 'Australia', 'Facebook', 'Published', '2026-02-03 10:09:27', 0, 0, NULL, 0, NULL, NULL),
+(7, NULL, '1770207153587-937416-vanessdev_Marseille_in_a_desirable_future_black_and_white_cin_fc2947f3-1740-42ee-bfc4-aedeef052256_2.mp4', 'dsdsvds', 'ddcdssdvsd', 'qssqqs', 'sqqsqs', '1770207153812-118295-sankaku_john_baldessari_art_--v_7_7d750db0-c595-4d24-8364-1efbdeeeced4.png', 'nl', 'Anguilla', 60, 'xdsdsqsqqs', 'Midjourney', 'sddssd', 'vanessa.biamonti@laplate.com', 'Juliette', 'Ravioli', 'Mr', '1986-11-20', '0650925487', NULL, '14 rue Exemple, Paris', 'Antigua and Barbuda', 'Facebook', 'Published', '2026-02-04 13:12:33', 0, 1, '2026-02-04 13:12:34', 1, '2026-02-04 13:12:34', NULL),
+(8, NULL, '1770288198272-43207-vanessdev_Marseille_in_a_desirable_future_black_and_white_cin_fc2947f3-1740-42ee-bfc4-aedeef052256_2.mp4', 'Pascale est bête', 'Pascale is stupid', 'on s\'en fiche', 'we don\'t care', '1770288198673-560183-malbich8_a_black_and_white_photo_of_an_angel_baby_cholo_wearing_0a86c5ec-2866-43a3-85d9-83766948d1f6.png', 'fr', 'France', 60, 'efgfegerger', 'Midjourney', 'gfergregregre', 'p.aubier@test.com', 'Pascale', 'Aubier', 'Mrs', '1973-05-16', '0650925487', NULL, '26 rue Exemple, Paris', 'France', 'Facebook', 'Published', '2026-02-05 11:43:18', 0, 1, '2026-02-05 11:43:20', 1, '2026-02-05 11:43:20', NULL),
+(9, NULL, '1770290692013-334990-vanessdev_futuristic_Provence_lavender_fields_autonomous_dron_f841c869-d7ab-44c5-ab70-1d5066da68a0_1.mp4', 'Test youtube API', 'Youtube API test', 'sdfvsddsf', 'dvsdfvdfdsf', '1770290692215-504971-hamcybele_photographic_grim_reaper_graphic_design_1960s_design__99877147-6258-4ce0-8450-15b594fb8db4.png', 'fr', 'Anguilla', 60, 'sdvsdfsdf', 'Midjourney', 'sddgfsdfd', 'g.gauliter@test.com', 'Ghislaine', 'Gaultier', 'Mrs', '1983-10-27', '0650925487', NULL, '15 rue Exemple, Paris', 'Australia', 'Facebook', 'Published', '2026-02-05 12:24:52', 0, 1, '2026-02-05 12:24:53', 1, '2026-02-05 12:24:53', NULL),
+(10, 'wcy5srKTzbk', '1770290989616-973820-gersto_A_dramatic_black-and-white_film-noir-style_scene_of_a__eed6a183-af7c-4864-ae6c-f81f9949c760_0.mp4', 'C\'est la bonne cette fois', 'This time it works', 'xvvsdvsd', 'svsdvsdv', '1770290989671-508681-sankaku_john_baldessari_art_--v_7_7d750db0-c595-4d24-8364-1efbdeeeced4.png', 'es', 'Albania', 60, 'xvvsccv', 'Midjourney', 'cvcsvc', 'g.fion@gmail.com', 'Gwenaelle', 'FION', 'Mrs', '1990-09-20', '0650925487', NULL, '14 rue Exemple, Paris', 'Algeria', 'Facebook', 'Published', '2026-02-05 12:29:49', 0, 1, '2026-02-05 12:29:53', 1, '2026-02-05 12:29:53', NULL);
 
 -- --------------------------------------------------------
 
@@ -409,7 +433,10 @@ INSERT INTO `video_subtitles` (`id`, `video_id`, `file_name`, `language`, `creat
 (2, 4, '1770022547060-458523-soustitre.srt', NULL, '2026-02-02 09:55:47'),
 (3, 5, '1770025470507-840368-soustitre.srt', NULL, '2026-02-02 10:44:30'),
 (4, 6, '1770037126209-761699-soustitre.srt', NULL, '2026-02-02 13:58:46'),
-(5, 7, '1770207153954-129470-soustitre.srt', NULL, '2026-02-04 13:12:34');
+(5, 7, '1770207153954-129470-soustitre.srt', NULL, '2026-02-04 13:12:34'),
+(6, 8, '1770288198747-822692-soustitre.srt', NULL, '2026-02-05 11:43:20'),
+(7, 9, '1770290692247-398099-soustitre.srt', NULL, '2026-02-05 12:24:53'),
+(8, 10, '1770290989733-348272-soustitre.srt', NULL, '2026-02-05 12:29:52');
 
 -- --------------------------------------------------------
 
@@ -421,6 +448,17 @@ CREATE TABLE `video_tag` (
   `video_id` int NOT NULL,
   `tag_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `video_tag`
+--
+
+INSERT INTO `video_tag` (`video_id`, `tag_id`) VALUES
+(8, 1),
+(9, 1),
+(10, 1),
+(9, 2),
+(10, 2);
 
 --
 -- Index pour les tables déchargées
@@ -502,10 +540,11 @@ ALTER TABLE `memo_selector`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `newsletter_subscriptions`
+-- Index pour la table `newsletter_subscribers`
 --
-ALTER TABLE `newsletter_subscriptions`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `newsletter_subscribers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Index pour la table `parameters`
@@ -636,10 +675,10 @@ ALTER TABLE `memo_selector`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `newsletter_subscriptions`
+-- AUTO_INCREMENT pour la table `newsletter_subscribers`
 --
-ALTER TABLE `newsletter_subscriptions`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `newsletter_subscribers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `parameters`
@@ -663,7 +702,7 @@ ALTER TABLE `social_media`
 -- AUTO_INCREMENT pour la table `still`
 --
 ALTER TABLE `still`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `tag`
@@ -675,7 +714,7 @@ ALTER TABLE `tag`
 -- AUTO_INCREMENT pour la table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `users`
@@ -687,13 +726,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `videos`
 --
 ALTER TABLE `videos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `video_subtitles`
 --
 ALTER TABLE `video_subtitles`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Contraintes pour les tables déchargées
@@ -773,6 +812,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-ALTER TABLE partner
-  MODIFY url VARCHAR(255) NULL;
