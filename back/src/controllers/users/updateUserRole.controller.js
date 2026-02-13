1c76f69da5515761772879d88c3218c25e951f8c
AnthonyPailloux <anthony.pailloux@laplateforme.io>
2026-02-13 09:55:31 +0100

    change_role_user

---
+++ back/src/controllers/users/updateUserRole.controller.js
@@ -0,0 +1,13 @@
+import { updateUserRoleService } from "../../services/users/updateUserRole.service.js";
+
+// changer le role de l'utilisateur
+export async function updateUserRoleController(req, res, next) {
+    try {
+        const { id } = req.params;
+        const { role } = req.body;
+        await updateUserRoleController(id, role);
+        res.json({ success: true, message: "Role updated" });
+    } catch (error) {
+        next(error);
+    }
+}
