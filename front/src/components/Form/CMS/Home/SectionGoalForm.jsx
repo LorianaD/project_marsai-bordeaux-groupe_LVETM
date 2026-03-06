2c4a51cdcb74080df6f4be594fed39c047c396d8
LorianaDIANO <128498370+LorianaD@users.noreply.github.com>
2026-03-06 12:58:48 +0100

    Fix import paths for icon images in SectionGoalForm

--- front/src/components/Form/CMS/Home/SectionGoalForm.jsx
+++ front/src/components/Form/CMS/Home/SectionGoalForm.jsx
@@ -1,6 +1,6 @@
 import CmsInput from "../Fields/CmsInput"
-import iconPaintDark from "../../../../assets/imgs/icones/iconPaintDark.svg";
-import iconPaint from "../../../../assets/imgs/icones/iconPaint.svg";
+import iconPaintDark from "../../../../assets/imgs/icones/IconPaintDark.svg";
+import iconPaint from "../../../../assets/imgs/icones/IconPaint.svg";
 import { useTranslation } from "react-i18next";
 import { useForm } from "../../../../hooks/useForm";
 import { useEffect, useState } from "react";
