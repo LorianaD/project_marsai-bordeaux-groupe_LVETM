639a51d90e9a4ae7632b0c65a5226f44e527745a
LorianaDIANO <128498370+LorianaD@users.noreply.github.com>
2026-03-06 12:58:04 +0100

    Fix icon import paths in SectionConceptForm.jsx

--- front/src/components/Form/CMS/Home/SectionConceptForm.jsx
+++ front/src/components/Form/CMS/Home/SectionConceptForm.jsx
@@ -1,6 +1,6 @@
 import CmsInput from "../Fields/CmsInput.jsx"
-import iconPaintDark from "../../../../assets/imgs/icones/iconPaintDark.svg";
-import iconPaint from "../../../../assets/imgs/icones/iconPaint.svg";
+import iconPaintDark from "../../../../assets/imgs/icones/IconPaintDark.svg";
+import iconPaint from "../../../../assets/imgs/icones/IconPaint.svg";
 import { useTranslation } from "react-i18next";
 import { useForm } from "../../../../hooks/useForm.js";
 import { useEffect, useState } from "react";
