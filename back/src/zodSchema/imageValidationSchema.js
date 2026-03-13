c27dfe654039aecdb9dce6f2af37b816396f3a49
edouard-todorov-formation <edouard.todorov@laplateforme.io>
2026-03-13 11:08:13 +0100

    zod_added_to_jury_routes_and_add_error_message_for_missing_file_on_zod_middleware

--- back/src/zodSchema/imageValidationSchema.js
+++ back/src/zodSchema/imageValidationSchema.js
@@ -25,5 +25,6 @@ export const fileImageSchema = z.object({
 
 //pour les images optionnelles
 export const optionalFileImageSchema = z.object({
-    file: imageSchema.optional(),
+    file: imageSchema
+       .optional(),
 });
