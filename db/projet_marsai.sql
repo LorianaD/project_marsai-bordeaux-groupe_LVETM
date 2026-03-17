7f5c1f84bc727d40140ef103e89dc0478eb019d4
LorianaDIANO <128498370+LorianaD@users.noreply.github.com>
2026-03-17 08:42:21 +0100

    Remove foreign key constraint from 'users' table
    Removed foreign key constraint from the 'users' table.

--- db/projet_marsai.sql
+++ db/projet_marsai.sql
@@ -1518,12 +1518,6 @@ ALTER TABLE `still`
   ADD CONSTRAINT `fk_still_video` FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`) ON DELETE CASCADE;
 
 --
--- Contraintes pour la table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id`) REFERENCES `cms` (`id`);
-
---
 -- Contraintes pour la table `video_review`
 --
 ALTER TABLE `video_review`
